9ae573805a718183d25a198eb9338936ed1bca21
Alexis Benoliel <alexbeno@users.noreply.github.com>
2026-06-02 11:51:42 +0200

    Add code view (#87)

--- tracer-viewer-bundle/assets/typescript.contribution-DwIxKTa7.js
+++
@@ -1,8 +0,0 @@
-/**
- * Copyright: (c) Myia SAS 2026.
- * This file and its contents are licensed under the AGPLv3 License.
- * Please see the LICENSE file at the root of this repository
- */
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/typescript-IOAorcXK.js","assets/editor.api2-AsNCLb-7.js","assets/editor-D3zK0gCL.css","assets/toggleHighContrast-CS8ieQzx.js","assets/toggleHighContrast-BGkXK-F6.css"])))=>i.map(i=>d[i]);
-import{_ as t}from"./index-BkYVdrtR.js";import{r}from"./_.contribution-BCyjvbUH.js";import"./editor.api2-AsNCLb-7.js";import"./toggleHighContrast-CS8ieQzx.js";r({id:"typescript",extensions:[".ts",".tsx",".cts",".mts"],aliases:["TypeScript","ts","typescript"],mimetypes:["text/typescript"],loader:()=>t(()=>import("./typescript-IOAorcXK.js"),__vite__mapDeps([0,1,2,3,4]))});
-//# sourceMappingURL=typescript.contribution-DwIxKTa7.js.map
